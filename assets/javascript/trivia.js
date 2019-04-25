856c958bc56ccf0cc6a5eed71430d047822e30df
Christian Mader <Cmader555@users.noreply.github.com>
2019-04-24 20:16:01 -0700

    js comments

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -2,7 +2,7 @@
 
 var questions = [{
     question: "Who does Michael Scott accidentally hit with his car in the parking lot? ",
-    choices: ["Meredith", "kevin", "Phyllis", "Oscar"],
+    choices: ["Meredith", "Kevin", "Phyllis", "Oscar"],
     answer: "Meredith"
 
 },
@@ -33,7 +33,7 @@ var questions = [{
     answer: "Justice-Beaver"
 }];
 
-//addtional variables needed
+//addtional variables needed for game to run properly 
 var correct = 0;
 var incorrect = 0;
 var unanswerred = 0;
@@ -44,54 +44,58 @@ questionIndex = 0;
 
 
 
-
+//intial start button to begin game via the function as well as hiding start button
 $(".startbutton").on("click", function () {
 
     $(this).hide();
-    beginGame(questionIndex);
+    game(questionIndex);
 
 })
 
-
+//timer function
 function timer() {
 
+    // if this occurs timer stops
     if (timeLeft === 0) {
         clearInterval(counter);
         //console.log("timer has stopped")
 
-
+        // if not at zero it ticks down to zero and displays time
     } else {
         timeLeft--;
         //console.log("Seconds Remaining:" + timeLeft);
         $(".timer").html("<p>Seconds Remaining: <p>" + timeLeft);
     };
+    //if time gets to zero, displays that you did not answer a question and continues game
     if (timeLeft === 0) {
         $(".answer").html(`<p> You did not select an answer before time ran out!</p>`);
         unanswerred++;
-       questionIndex++
-        setTimeout(function () { beginGame(questionIndex) }, 3000);
+        questionIndex++
+        setTimeout(function () { game(questionIndex) }, 3000);
         clearInterval(counter);
     }
 
 };
 
-function beginGame(index) {
 
+//allows game to procede 
+function game(index) {
+    //empties answer and choices display in case something is already there
     $(".answer").empty();
     $(".choices").empty();
 
     if (index < questions.length) {
-
+        // if index is smaller then question length timer begins
         timeLeft = 16;
         counter = setInterval(timer, 1000)
-
+        //dispalys question at current index
         $(".question").html("<p>" + questions[index].question + "</p>");
-
+        // at current question index, for each choice index a button is created ad appened to the last, and a value of said choice index is added to each button as they are "created"
         for (i = 0; i < questions[index].choices.length; i++) {
             $(".choices").append(`<button type='button' class='btn btn-dark choicebtn' value=${questions[index].choices[i]}>  ${questions[index].choices[i]}  </button>`);
         }
     } else {
-
+        // if index becomes larger then questions.length finall correct/inccorrect/unanswerred amount shows and restart function is invoked
         $(".qright").show();
         $(".qwrong").show();
         $(".qunanswered").show();
@@ -103,17 +107,18 @@ function beginGame(index) {
         restart();
 
     }
-    
-};
 
+};
+//if any of the ".choicebtns" are clicked the following will happen
 $(document).on("click", ".choicebtn", function () {
+    // answerGuessed set to value of button clicked
     var answerGuessed = $(this).attr("value")
     //console.log("Answer is" + answerGuessed)
-
+    // if value of button clicked is equal to answewr at current index, correct count is increased and correct HTML displayed
     if (answerGuessed === questions[questionIndex].answer) {
         $(".answer").html(`<p> You got the correct Answer! </p>`);
         correct++;
-
+        //if correct answer not selected.. IE they do not equal, then icorrect value increases and incorrect message is displayed on DOM
     } else {
         $(".answer").html(`<p> You guessed the wrong Answer! Try again next time! </p>`);
         incorrect++;
@@ -121,16 +126,17 @@ $(document).on("click", ".choicebtn", function () {
 
     }
 
-    console.log(timeLeft);
+    //console.log(timeLeft);
 
-
+    //regardeless of what is clicked question index increases so questions/choices/answer will continue until index is no longer less then question.length see approx line 87
     questionIndex++;
-
-    setTimeout(function () { beginGame(questionIndex) }, 3000);
+    //waits 3 seconds before game function invoked, leading to next question
+    setTimeout(function () { game(questionIndex) }, 3000);
+    //clears interval, because if not cleared mutiple intervals will be invoked each time a new question is genrated, exponentiall increasing countdown from 16
     clearInterval(counter);
 });
 
-
+//restart function--invoked at end of game function, will be invoked at the end of each game once index is no longer smaller then question.length, start button shows, and when clicked it resets all variables necessary, and game function called again. 
 function restart() {
 
     $(".startbutton").show();
@@ -151,7 +157,7 @@ function restart() {
         $(".qunanswered").hide();
         $(this).hide();
 
-        beginGame(questionIndex);
+        game(questionIndex);
 
 
     });
